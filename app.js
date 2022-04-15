626305dd174cec15e359fa26a81f1b0a19cefc4b
Felix Waller <felixwaller.public@gmail.com>
2022-04-15 19:43:35 +0100

    Commit

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 console.log("Dedicated to Madi");
 
 // Read stored name
-const stored_name = localStorage.getItem("name");
-var name_input = document.querySelector("#name");
-name_input.value = stored_name;
+var nameInput = document.getElementById("name");
+nameInput.value = localStorage.getItem("name");
 
 // Display date
-const dateObj = new Date();
-document.querySelector("#date").textContent = dateObj.toISOString().slice(0, 10);
+var dateObj = new Date();
+document.getElementById("date").textContent = dateObj.toISOString().slice(0, 10);
 
 // Store name locally every second
 setInterval(() => {
-    localStorage.setItem("name", name_input.value);
+    localStorage.setItem("name", nameInput.value);
 }, 1000);
 
-// Achievement entry logic
-var lines = 1
+// Achievement editor
+var lines = 1;
 var achievements = document.getElementById("achievements");
-achievements.addEventListener("keypress", key_pressed);
+achievements.addEventListener("keydown", keydown);
 
-// Submit button logic
+// Submit button
 var isSubmitted = false;
 var button = document.querySelector("button");
-button.addEventListener("click", submit_clicked);
+button.addEventListener("click", submit);
 
-function key_pressed(e) {
+function keydown(e) {
     if (e.key == "Enter") {
         lines++;
         achievements.insertAdjacentHTML(
@@ -34,27 +33,26 @@ function key_pressed(e) {
                 <input type="text" name="${lines}" id="${lines}" placeholder=" ">
             </div>`
         );
-        let inputs = document.querySelectorAll("#achievements input");
-        inputs[inputs.length - 1].focus();
+        document.getElementById(lines).focus();
     }
 }
 
-function submit_clicked() {
+function submit() {
     if (!isSubmitted) {
         isSubmitted = true;
 
         // Create spinner and msg
-        const msgs = ["Excellent work", "You're on fire", "Godlike", "Pop off", "Good stuff"];
+        const MSGS = ["Excellent work", "You're on fire", "Godlike", "Pop off", "Good stuff"];
         document.querySelector("body").insertAdjacentHTML(
             "beforeend", 
             `<div id="spinner"></div>
-            <p id="msg">${msgs[Math.floor(Math.random() * msgs.length)]}</p>`
+            <p id="msg">${MSGS[Math.floor(Math.random() * MSGS.length)]}</p>`
         );
         let msg = document.getElementById("msg");
         msg.hidden = true;
 
         // Adjust msg if user entered a name
-        let name = name_input.value;
+        let name = nameInput.value;
         if (name) {
             msg.textContent += `, ${name}!`;
         } else {

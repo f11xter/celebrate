6f6219fa6d8c3ac27b551ac17dcc2789def22399
Felix Waller <felixwaller.public@gmail.com>
2022-05-03 23:57:35 +0100

    Add clear button Fix name formatting Add noscript warning

--- app.js
+++ app.js
@@ -18,16 +18,23 @@ var lines = 1;
 var achievements = document.getElementById("achievements");
 achievements.addEventListener("keydown", keydown);
 
+// Celebrate msg
+var msg = document.getElementById("msg");
+
 // Submit button
 var isSubmitted = false;
-var button = document.querySelector("button");
-button.addEventListener("click", submit);
+document.getElementById("submit").addEventListener("click", submit);
+
+// Clear button
+var isCleared = false;
+document.getElementById("clear").addEventListener("click", clear);
 
 function keydown(e) {
+    isCleared = false;
     if (e.key == "Enter") {
         lines++;
         achievements.insertAdjacentHTML(
-            "beforeend", 
+            "beforeend",
             `<div class="flex-container" id="div${lines}">
                 <label for="${lines}">${lines}.</label>
                 <input type="text" name="${lines}" id="${lines}" placeholder=" ">
@@ -40,29 +47,66 @@ function keydown(e) {
 function submit() {
     if (!isSubmitted) {
         isSubmitted = true;
+        isCleared = false;
 
         // Create spinner and msg
         const MSGS = ["Excellent work", "You're on fire", "Godlike", "Pop off", "Good stuff"];
         document.querySelector("body").insertAdjacentHTML(
-            "beforeend", 
-            `<div id="spinner"></div>
-            <p id="msg">${MSGS[Math.floor(Math.random() * MSGS.length)]}</p>`
+            "beforeend",
+            `<div id="spinner"></div>`
         );
-        let msg = document.getElementById("msg");
-        msg.hidden = true;
-
-        // Adjust msg if user entered a name
-        let name = nameInput.value;
-        if (name) {
-            msg.textContent += `, ${name}!`;
-        } else {
-            msg.textContent += "!";
-        }
 
         // Remove spinner and show msg after 4 seconds
         setTimeout(() => {
-            document.getElementById("spinner").hidden = true;
-            msg.hidden = false;
+            let spinner = document.getElementById("spinner");
+            if (spinner) {
+                spinner.remove();
+            }
+
+            if (!isCleared) {
+                // Adjust msg if user entered a name
+                let name = nameInput.value;
+                let msgContent = MSGS[Math.floor(Math.random() * MSGS.length)];
+                if (name) {
+                    msgContent += `, ${name}!`;
+                } else {
+                    msgContent += "!";
+                }
+
+                msg.textContent = msgContent;
+            }
         }, 4000);
     }
+}
+
+function clear() {
+    isCleared = true;
+    isSubmitted = false;
+
+    // Remove msg
+    msg.textContent = "";
+
+    // Remove spinner
+    let spinner = document.getElementById("spinner");
+    if (spinner) {
+        spinner.remove();
+    }
+
+    // Remove a line every 100ms
+    let clear = setInterval(() => {
+        document.getElementById(`div${lines}`).remove();
+        lines--;
+
+        if (lines < 1) {
+            lines = 1;
+            clearInterval(clear);
+            achievements.insertAdjacentHTML(
+                "beforeend",
+                `<div class="flex-container" id="div1">
+                    <label for="1">1.</label>
+                    <input type="text" name="1" id="1" placeholder="First achievement...">
+                </div>`
+            );
+        }
+    }, 100);
 }
